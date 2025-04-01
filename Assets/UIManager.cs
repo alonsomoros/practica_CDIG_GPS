a6ece1e3071edc58a93e02359a7739ba30d336f7
Alon <alonsomorosvillalba@gmail.com>
2025-04-02 01:37:54 +0200

    Implementada ventana Registro (falta consumoCoche) y implementado sistema de Divs para óptima estructura

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using System.Collections.Generic;
 
 public class UIManager : MonoBehaviour
 {
-    public List<InputField> inputFields;
-    //public Slider slider;
-    //public Scrollbar scrollbar;
+    public List<TMP_InputField> inputFields, inputFieldsObligatorios;
     public Canvas canvasLogin, canvasRegistro, canvasCreditos, canvasNavegacion, canvasDestinosFav;
-    public Button IS_botonOlvidadoContrasena, IS_botonLogin, IS_botonRegistro, IS_botonCreditos, C_botonVolverDeCreditos;
+    public Button IS_botonOlvidadoContrasena, IS_botonLogin, IS_botonRegistro, IS_botonCreditos, 
+    C_botonVolverDeCreditos, 
+    R_botonVolverDeRegistro, R_botonSiguientePaso1, R_botonVolverPaso2, R_botonSiguientePaso2, R_botonVolverPaso3, R_botonTerminarPaso3;
+
+    public GameObject R_panelPaso1, R_panelPaso2, R_panelPaso3;
+
+    public TMP_Text textoObligatorioNombre, textoObligatorioApellidos, textoObligatorioEmail, textoObligatorioContrasena, textoObligatorioRepetirContrasena,
+    indicePaso1, indicePaso2, indicePaso3;
 
     void Start()
     {
@@ -21,62 +27,204 @@ public class UIManager : MonoBehaviour
             Debug.Log("Se está ejecutando.");
         }
 
-        canvasLogin.enabled=true;
-        canvasRegistro.enabled=false;
-        canvasCreditos.enabled=false;
-        canvasNavegacion.enabled=false;
-        canvasDestinosFav.enabled=false;
+        foreach (var input2 in inputFieldsObligatorios)
+        {
+            input2.onEndEdit.AddListener(ConfirmText);
+        }
+        if (Application.isPlaying)
+        {
+            Debug.Log("Se está ejecutando.");
+        }
 
-        IS_botonOlvidadoContrasena.onClick.AddListener(ISaR_Boton_InicioSesion_OlvidadoContrasena);
-        IS_botonCreditos.onClick.AddListener(ISaC_Boton_InicioSesion_Creditos);
-        IS_botonRegistro.onClick.AddListener(ISaR_Boton_InicioSesion_Registro);
-        IS_botonLogin.onClick.AddListener(ISaN_Boton_InicioSesion_Navegacion);
-        C_botonVolverDeCreditos.onClick.AddListener(CaIS_Boton_Creditos_Volver_InicioSesion);
+        // Deshabilitar todos los Canvas inicialmente
+        InicializarCanvas();
+        InicializarPaneles();
 
-    }
-    public void ISaC_Boton_InicioSesion_Creditos()
-    {
-        canvasLogin.enabled=false;
-        canvasRegistro.enabled=false;
-        canvasCreditos.enabled=true;
-        canvasNavegacion.enabled=false;
-        canvasDestinosFav.enabled=false;
+        // Configurar eventos para los botones
+        ConfigurarEventosBotones();
     }
 
-    public void ISaR_Boton_InicioSesion_Registro()
+    private void InicializarCanvas()
     {
-        canvasLogin.enabled=false;
-        canvasRegistro.enabled=true;
-        canvasCreditos.enabled=false;
-        canvasNavegacion.enabled=false;
-        canvasDestinosFav.enabled=false;
+    canvasLogin.enabled = true;
+    canvasRegistro.enabled = false;
+    canvasCreditos.enabled = false;
+    canvasNavegacion.enabled = false;
+    canvasDestinosFav.enabled = false;
     }
 
-    public void ISaN_Boton_InicioSesion_Navegacion()
+    private void InicializarPaneles()
     {
-        canvasLogin.enabled=false;
-        canvasRegistro.enabled=false;
-        canvasCreditos.enabled=false;
-        canvasNavegacion.enabled=true;
-        canvasDestinosFav.enabled=false;
+        R_panelPaso1.SetActive(true);
+        R_panelPaso2.SetActive(false);
+        R_panelPaso3.SetActive(false);
     }
 
-    public void ISaR_Boton_InicioSesion_OlvidadoContrasena()
-    {
-        canvasLogin.enabled=false;
-        canvasRegistro.enabled=true;
-        canvasCreditos.enabled=false;
-        canvasNavegacion.enabled=false;
-        canvasDestinosFav.enabled=false;
+    private void ConfigurarEventosBotones() 
+    {   
+    IS_botonOlvidadoContrasena.onClick.AddListener(ISaR_Boton_InicioSesion_OlvidadoContrasena_a_Registro);
+    IS_botonLogin.onClick.AddListener(ISaN_Boton_InicioSesion_a_Navegacion);
+    IS_botonRegistro.onClick.AddListener(ISaR_Boton_InicioSesion_a_Registro);
+    IS_botonCreditos.onClick.AddListener(ISaC_Boton_InicioSesion_a_Creditos);
+
+    C_botonVolverDeCreditos.onClick.AddListener(CaIS_Boton_Creditos_Volver_a_InicioSesion);
+
+    R_botonVolverDeRegistro.onClick.AddListener(RaIS_Boton_Registro_Volver_a_InicioSesion);
+    R_botonSiguientePaso1.onClick.AddListener(R_Botón_Siguiente_Verificar_Campos_Paso1);
+    R_botonVolverPaso2.onClick.AddListener(R_Boton_Volver_Paso2);    
+    R_botonSiguientePaso2.onClick.AddListener(R_Boton_Siguiente_Paso2);
+    R_botonVolverPaso3.onClick.AddListener(R_Boton_Volver_Paso3);
+    R_botonTerminarPaso3.onClick.AddListener(R_Boton_Terminar_Paso3);
     }
 
-    public void CaIS_Boton_Creditos_Volver_InicioSesion()
+    public void ISaR_Boton_InicioSesion_OlvidadoContrasena_a_Registro()
     {
-        canvasLogin.enabled = true;
+        R_panelPaso1.SetActive(true);
+        indicePaso1.color = Color.green;
+        indicePaso2.color = Color.white;
+        indicePaso3.color = Color.white;
+        cambiarCanvas(canvasRegistro);
+    }
+
+    public void ISaN_Boton_InicioSesion_a_Navegacion()
+    {
+        cambiarCanvas(canvasNavegacion);
+    }
+    
+    public void ISaR_Boton_InicioSesion_a_Registro()
+    {
+        R_panelPaso1.SetActive(true);
+        indicePaso1.color = Color.green;
+        indicePaso2.color = Color.white;
+        indicePaso3.color = Color.white;
+        cambiarCanvas(canvasRegistro);
+    }
+
+    public void ISaC_Boton_InicioSesion_a_Creditos()
+    {
+        cambiarCanvas(canvasCreditos);
+    }
+
+
+    public void CaIS_Boton_Creditos_Volver_a_InicioSesion()
+    {
+        cambiarCanvas(canvasLogin);
+    }
+
+
+    public void RaIS_Boton_Registro_Volver_a_InicioSesion()
+    {
+        cambiarCanvas(canvasLogin);
+    }
+
+    private void R_Botón_Siguiente_Verificar_Campos_Paso1()
+    {
+        bool todosRellenados = true;
+
+        foreach (var inputField in inputFieldsObligatorios)
+        {
+
+            Debug.Log("Procesando campo: " + inputField.name);
+
+            if (string.IsNullOrWhiteSpace(inputField.text)) // Comprobar si el campo está vacío
+            {
+                todosRellenados = false;
+
+                // Cambiar el color del borde o texto del InputField a rojo
+                if (ColorUtility.TryParseHtmlString("#FFCCCC", out Color softRed))
+                {
+                    inputField.image.color = softRed; // Cambiar el fondo a un rojo suave
+                }
+            }
+            else
+            {
+                // Restaurar el color original si el campo está rellenado
+                inputField.image.color = Color.white; // Cambia al color original
+            }
+        }
+
+        // Verificar que las contraseñas coincidan
+        if (textoObligatorioContrasena.text != textoObligatorioRepetirContrasena.text)
+        {
+            todosRellenados = false;
+
+            // Cambiar el color de los campos de contraseña a rojo
+            if (ColorUtility.TryParseHtmlString("#bf4934", out Color softRed_v2))
+            {
+                textoObligatorioContrasena.color = softRed_v2;
+                textoObligatorioRepetirContrasena.color = softRed_v2;
+            }
+
+            Debug.Log("Las contraseñas no coinciden.");
+
+        }
+        else
+        {
+            // Restaurar el color original si las contraseñas coinciden
+            textoObligatorioContrasena.color = Color.black;
+            textoObligatorioRepetirContrasena.color = Color.black;
+        }
+        
+
+        if (todosRellenados)
+        {
+            // Avanzar al siguiente panel si todos los campos están rellenados
+            R_panelPaso1.SetActive(false);
+            R_panelPaso2.SetActive(true);
+            indicePaso2.color = Color.green;
+            indicePaso1.color = Color.white;
+        }
+        else
+        {
+            Debug.Log("Por favor, rellena todos los campos obligatorios.");
+        }
+    }
+
+    public void R_Boton_Volver_Paso2()
+    {
+        R_panelPaso1.SetActive(true);
+        R_panelPaso2.SetActive(false);
+        indicePaso1.color = Color.green;
+        indicePaso2.color = Color.white;
+    }
+
+    public void R_Boton_Siguiente_Paso2()
+    {
+        R_panelPaso2.SetActive(false);
+        R_panelPaso3.SetActive(true);
+        indicePaso2.color = Color.white;
+        indicePaso3.color = Color.green;
+    }
+
+    public void R_Boton_Volver_Paso3()
+    {
+        R_panelPaso2.SetActive(true);
+        R_panelPaso3.SetActive(false);
+        // Cambiar el color a verde
+        indicePaso2.color = Color.green;
+        indicePaso3.color = Color.white;
+    }
+
+    public void R_Boton_Terminar_Paso3()
+    {
+        R_panelPaso3.SetActive(false);
+        // Aquí puedes agregar la lógica para finalizar el registro o volver a la pantalla de inicio de sesión
+        cambiarCanvas(canvasLogin);
+        resetAll(); // Reiniciar todos los campos al finalizar el registro
+    }
+
+    public void cambiarCanvas(Canvas canvas)
+    {
+        canvasLogin.enabled = false;
         canvasRegistro.enabled = false;
         canvasCreditos.enabled = false;
         canvasNavegacion.enabled = false;
         canvasDestinosFav.enabled = false;
+
+        if (canvas != null)
+        {
+            canvas.enabled = true;
+        }
     }
 
     void ConfirmText(string text)
@@ -84,4 +232,18 @@ public class UIManager : MonoBehaviour
         Debug.Log("Texto final confirmado: " + text);
     }
 
+    public void resetAll()
+    {
+        foreach (var input in inputFields)
+        {
+            input.text = string.Empty;
+            input.image.color = Color.white; // Cambia al color original
+        }
+
+        foreach (var input2 in inputFieldsObligatorios)
+        {
+            input2.text = string.Empty;
+            input2.image.color = Color.white; // Cambia al color original
+        }
+    }
 }
